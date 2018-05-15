e7a43397ee8ffd04b4097a5b01d6cda0362455ab
himanshu-7 <h.shah@student.tudelft.nl>
2018-05-15 22:40:09 +0200

    added function to make joystick control packet

--- pc_terminal/js.c
+++ pc_terminal/js.c
@@ -8,20 +8,27 @@
 #include <stdio.h>
 #include <errno.h>
 #include <string.h>
+#include <time.h>
+#include <assert.h>
 
 #include "joystick.h"
+#include "../crc.h"
+#include "protocol.h"
+
+#define JS_DEV	"/dev/input/js0"
 
 
 /* current axis and button readings
  */
+
+int fd;
+struct js_event js;
+struct packet j_obj;
 int	axis[6];
+char new_axis[6];
 int	button[12];
 
 
-/* time
- */
-#include <time.h>
-#include <assert.h>
 unsigned int    mon_time_ms(void)
 {
         unsigned int    ms;
@@ -34,6 +41,31 @@ unsigned int    mon_time_ms(void)
         return ms;
 }
 
+void j_scale(int axis[])
+{
+
+	unsigned int range = 65535;
+	unsigned int new_range = 255;
+	unsigned int max = 32767;
+	int min = -32768;
+	int new_min = -128;
+	
+	for(int i = 0; i < 6; i++)
+	{		
+		if(axis[i] >= min && axis[i] <= max)
+		{
+			if (axis[i] == 0) 
+			{
+				new_axis[i] = 0;
+			} 
+			else 
+			{
+				new_axis[i] = (((axis[i] - min) * new_range) / range) + new_min;
+			}
+		}
+	}
+}
+
 void    mon_delay_ms(unsigned int ms)
 {
         struct timespec req, rem;
@@ -43,14 +75,7 @@ void    mon_delay_ms(unsigned int ms)
         assert(nanosleep(&req,&rem) == 0);
 }
 
-
-#define JS_DEV	"/dev/input/js0"
-
-int main (int argc, char **argv)
-{
-	int 		fd;
-	struct js_event js;
-	unsigned int	t, i;
+void init_js(){
 
 	if ((fd = open(JS_DEV, O_RDONLY)) < 0) {
 		perror("jstest");
@@ -60,49 +85,70 @@ int main (int argc, char **argv)
 	/* non-blocking mode
 	 */
 	fcntl(fd, F_SETFL, O_NONBLOCK);
+}
 
-	while (1) {
+void make_j_packet(){
+	j_obj.header = J_CONTROL;
+	j_obj.crc8 = make_crc8_nontabled(J_CONTROL, &new_axis[0], 4);
+	rs232_putchar(j_obj.header);
+	for (uint8_t i = 0; i < 4; i++) {
+		rs232_putchar(new_axis[i]);
+	}
+	rs232_putchar(j_obj.crc8);
+	
+
+		//printf("\n<exit>\n");
+	}
+
+
+void send_j_packet()
+{
+	
+
+	//while (1) {
 
 
 		/* simulate work
 		 */
-		mon_delay_ms(300);
-		t = mon_time_ms();
+	//	mon_delay_ms(300);
+	//	t = mon_time_ms();
 
 		/* check up on JS
 		 */
-		while (read(fd, &js, sizeof(struct js_event)) == 
-		       			sizeof(struct js_event))  {
-
-			/* register data
-			 */
-			// fprintf(stderr,".");
-			switch(js.type & ~JS_EVENT_INIT) {
-				case JS_EVENT_BUTTON:
-					button[js.number] = js.value;
-					break;
-				case JS_EVENT_AXIS:
-					axis[js.number] = js.value;
-					break;
-			}
+	while (read(fd, &js, sizeof(struct js_event)) == 
+	       			sizeof(struct js_event))  {
+		/* register data
+		 */
+		// fprintf(stderr,".");
+		switch(js.type & ~JS_EVENT_INIT) {
+			case JS_EVENT_BUTTON:
+				button[js.number] = js.value;
+				break;
+			case JS_EVENT_AXIS:
+				axis[js.number] = js.value;
+				break;
 		}
-		if (errno != EAGAIN) {
-			perror("\njs: error reading (EAGAIN)");
-			exit (1);
-		}
-
-		printf("\n");
-		printf("%5d   ",t);
-		for (i = 0; i < 6; i++) {
-			printf("%6d ",axis[i]);
-		}
-		printf(" |  ");
-		for (i = 0; i < 12; i++) {
-			printf("%d ",button[i]);
-		}
-		if (button[0])
-			break;
 	}
-	printf("\n<exit>\n");
+	if (errno != EAGAIN) {
+		perror("\njs: error reading (EAGAIN)");
+		exit (1);
+	}
+	
+	j_scale(&axis[0]);									//scale down joystick axes values to one byte
 
+	if (button[0] || button[1]){
+		term_puts("Entering PANIC mode.....");			//If safe mode or panic mode button pressed, go to panic mode(which will automatically end up in safe mode)			
+		j_obj.header=MODESET;
+		j_obj.data=PANIC;
+		j_obj.crc8=0x00;								//TODO: compute CRC using function
+		rs232_putchar(j_obj.header);
+		rs232_putchar(j_obj.data);
+		rs232_putchar(j_obj.crc8);
+		return;;											//TODO: convert into a function and replace with a return 
+	}
+
+	if(abs(axis[0] >= 20)){
+		make_j_packet();
+	}
+	
 }
